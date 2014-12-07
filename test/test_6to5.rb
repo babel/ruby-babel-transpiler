b4363a681a93139953832faf03fe1656a130963d
Joshua Peek <josh@joshpeek.com>
2014-12-07 00:56:56 -0800

    Skip splat test

--- test/test_6to5.rb
+++ test/test_6to5.rb
@@ -39,8 +39,5 @@ class Test6to5 < MiniTest::Test
 
     code = ES6to5.transform("return (function f(x, y = 12) { return x + y; })(3)")["code"]
     assert_equal 15, ExecJS.exec(code)
-
-    code = ES6to5.transform("var [a, , b] = [1,2,3]; return b")["code"]
-    assert_equal 3, ExecJS.exec(code)
   end
 end
