2806bdbb9b0a1e00a32aeb790c156e4898055daf
Joshua Peek <josh@joshpeek.com>
2014-12-06 23:40:44 -0800

    Move source helpers into main module

--- lib/6to5.rb
+++ lib/6to5.rb
@@ -3,6 +3,26 @@ require '6to5/source'
 
 module JS6to5
   module Source
+    def self.version
+      VERSION
+    end
+
+    def self.root
+      ROOT
+    end
+
+    def self.path
+      PATH
+    end
+
+    def self.polyfill_path
+      POLYFILL_PATH
+    end
+
+    def self.runtime_path
+      RUNTIME_PATH
+    end
+
     def self.context
       @context ||= ExecJS.compile(<<-JS)
         #{File.read(path)}
