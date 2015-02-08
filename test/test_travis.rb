67494189db14b8d543a0866eb8565858ac303fff
Joshua Peek <josh@joshpeek.com>
2015-02-08 15:26:19 -0800

    Allow failures for travis linting

--- test/test_travis.rb
+++ test/test_travis.rb
@@ -1,18 +1,28 @@
 require 'minitest/autorun'
-require 'yaml'
 
-class TestTravis < MiniTest::Test
-  def test_env_source_version_matrix
-    config = YAML.load(File.read(".travis.yml"))
+if ENV['VALIDATE_TRAVIS']
+  require 'json'
+  require 'open-uri'
+  require 'set'
+  require 'yaml'
 
-    expected = File.read("source-versions.txt").split("\n")
-      .map { |v| v.sub(/^(\d+)\.(\d+)\.(\d+)$/, '~>\1.\2.0') }
-      .uniq
+  class TestTravis < MiniTest::Test
+    def test_env_source_version_matrix
+      url = "https://rubygems.org/api/v1/versions/6to5-source.json"
+      published = version_constraints(JSON.parse(open(url).read).map { |v| v["number"] })
 
-    actual = config["env"].map { |s| s[/^SOURCE_VERSION="(.+)"$/, 1] }.compact
+      local = version_constraints(File.read("source-versions.txt").split("\n"))
 
-    missing = expected - actual
-    assert_empty missing, "Add the following envs to .travis.yml: \n" +
-      missing.map { |v| "- SOURCE_VERSION=\"#{v}\"" }.join("\n")
+      config = YAML.load(File.read(".travis.yml"))
+      travis = Set.new(config["env"].map { |s| s[/^SOURCE_VERSION="(.+)"$/, 1] }.compact)
+
+      missing = (published & local) - travis
+      assert_empty missing, "Add the following envs to .travis.yml: \n" +
+        missing.map { |v| "- SOURCE_VERSION=\"#{v}\"" }.join("\n")
+    end
+
+    def version_constraints(versions)
+      Set.new(versions.compact.map { |v| v.sub(/^(\d+)\.(\d+)\.(\d+)$/, '~>\1.\2.0') })
+    end
   end
 end
